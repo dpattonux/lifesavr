aced14008e958574af951671b0c7ea1a26f9cbb6
dpattonux <danielnpatton@gmail.com>
2021-04-16 15:30:57 -0400

    added html and css for details step one screen

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./App";
-import Details2 from "./components/Details2";
+// import Details2 from "./components/Details2";
+import Details1 from "./components/Details1";
 // import SideBar from './components/SideBar';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Details2 />
+    <Details1 />
     {/* <SideBar/> */}
   </React.StrictMode>,
   document.getElementById("root")
