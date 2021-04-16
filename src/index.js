106fd3f302b222cab54bba7bf181eeb513c9e624
dpattonux <danielnpatton@gmail.com>
2021-04-16 11:04:20 -0400

    added a small charge

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-// import Details2 from "./components/Details2";
+// import App from "./App";
+import Details2 from "./components/Details2";
 // import SideBar from './components/SideBar';
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    {/* <Details2 /> */}
+    {/* <App /> */}
+    <Details2 />
     {/* <SideBar/> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
-
