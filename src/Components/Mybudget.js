28dc83beb9b1f1a12d5c9376ad5be2a963f4f0c4
dpattonux <danielnpatton@gmail.com>
2021-04-16 18:58:24 -0400

    small change

---
+++ src/Components/Mybudget.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SideBar from "./SideBar";
+
+class Mybudget extends Component {
+  render() {
+    return (
+      <>
+        <SideBar />
+      </>
+    );
+  }
+}
+
+export default Mybudget;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./App";
-import Details2 from "./components/Details2";
+// import Details2 from "./components/Details2";
+import Mybudget from "./components/Mybudget";
 // import Details1 from "./components/Details1";
 // import SideBar from './components/SideBar';
 
@@ -10,7 +11,8 @@ ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <Details1 /> */}
-    <Details2 />
+    {/* <Details2 /> */}
+    <Mybudget />
     {/* <SideBar/> */}
   </React.StrictMode>,
   document.getElementById("root")
