12ca8aae3e1ad89f082344a8825b126c1793ed7c
dpattonux <danielnpatton@gmail.com>
2021-04-16 22:02:19 -0400

    trying to fix this damn merge

--- src/index.js
+++ src/index.js
@@ -3,19 +3,30 @@ import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./App";
 // import Details2 from "./components/Details2";
+<<<<<<< HEAD
 import Mybudget from "./components/Mybudget";
 // import Details1 from "./components/Details1";
 // import SideBar from './components/SideBar';
 import BudgetSelections from "./components/BudgetSelections";
+=======
+// import Details1 from "./components/Details1";
+// import SideBar from './components/SideBar';
+import BudgetSelections from "./Components/BudgetSelections";
+>>>>>>> cd1d996 (Created the budget selections screen)
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <Details1 /> */}
     {/* <Details2 /> */}
+<<<<<<< HEAD
     <Mybudget />
     {/* <SideBar/> */}
     <BudgetSelections />
+=======
+    {/* <SideBar/> */}
+    <BudgetSelections/>
+>>>>>>> cd1d996 (Created the budget selections screen)
   </React.StrictMode>,
   document.getElementById("root")
 );
