708c5a691766d358f1956b55d14edc4c2f073b66
dpattonux <danielnpatton@gmail.com>
2021-04-16 20:35:14 -0400

    adding small changes

--- src/index.js
+++ src/index.js
@@ -3,17 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./App";
 // import Details2 from "./components/Details2";
+<<<<<<< HEAD
 import Mybudget from "./components/Mybudget";
+=======
+>>>>>>> cd1d996 (Created the budget selections screen)
 // import Details1 from "./components/Details1";
 // import SideBar from './components/SideBar';
+import BudgetSelections from "./components/BudgetSelections";
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <Details1 /> */}
     {/* <Details2 /> */}
+<<<<<<< HEAD
     <Mybudget />
+=======
+>>>>>>> cd1d996 (Created the budget selections screen)
     {/* <SideBar/> */}
+    <BudgetSelections/>
   </React.StrictMode>,
   document.getElementById("root")
 );
